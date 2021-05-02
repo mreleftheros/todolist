419a80bb586c6528f8910fb194202002361d8d05
eleftheros <eleftheros_all@protonmail.com>
2021-05-02 17:01:26 +0300

    fix pointer events in ::after div

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 const addForm = document.getElementById("addForm");
+let todos;
+
+// check localStorage for todos
+function checkLocalStorage () {
+  if(localStorage.todos)
+    todos = localStorage.getItem(todos);
+
+}
 
 //event listeners
+window.addEventListener("DOMContentLoaded", checkLocalStorage);
+
 addForm.addEventListener("submit", e => {
   e.preventDefault();
 
